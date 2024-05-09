f21a11555a4da846b6ff33620777b7951a954ae6
salado-jofel <saladojofel@gmail.com>
2024-05-09 18:56:02 +0800

    fix: changed route in a href

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,7 +1,9 @@
 <x-layout>
     <div class="w-full h-full flex justify-start items-center flex-col gap-[30px]">
         <div class=" border px-[20px] py-[10px] border-black text-[24px]">
-            <h1 class="">New Note</h1>
+            <a href="{{ route('note.create') }}">
+                <h1 class="">New Note</h1>
+            </a>
         </div>
         @foreach ($notes as $note)
             <div class="flex flex-col justify-center items-center bg-slate-500 rounded-md shadow-2xl overflow-hidden">
@@ -10,12 +12,22 @@
                 </div>
                 <div class="h-[100px] flex justify-center items-center">
                     <div class="flex flex-row gap-[20px] text-white">
+
+                        <a href={{ route('note.show', $note) }}>
+                            <button
+                                class="bg-blue-500
+                            px-[15px] py-[8px] rounded-md shadow-2xl cursor-pointer scale-100 hover:scale-125
+                            duration-300">VIEW</button>
+                        </a>
+                        <a href={{ route('note.edit', $note) }}>
+                            <button
+                                class="bg-green-500
+                            px-[15px] py-[8px] rounded-md shadow-2xl scale-100 hover:scale-125
+                            duration-300">EDIT</button>
+                        </a>
                         <button
-                            class="bg-blue-500 px-[15px] py-[8px] rounded-md shadow-2xl cursor-pointer scale-100 hover:scale-125 duration-300">VIEW</button>
-                        <button
-                            class="bg-green-500 px-[15px] py-[8px] rounded-md shadow-2xl scale-100 hover:scale-125 duration-300">EDIT</button>
-                        <button
-                            class="bg-red-500 px-[15px] py-[8px] rounded-md shadow-2xl scale-100 hover:scale-125 duration-300">DELETE</button>
+                            class="bg-red-500 px-[15px]
+                            py-[8px] rounded-md shadow-2xl scale-100 hover:scale-125 duration-300">DELETE</button>
                     </div>
                 </div>
 
